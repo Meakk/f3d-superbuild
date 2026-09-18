b6d29a12b944eaf7a7fba8beb28248f757607da0
Michael Migliore <mcmigliore@gmail.com>
2026-09-18 23:58:57 +0200

    try windows

--- projects/ffmpeg.cmake
+++ projects/ffmpeg.cmake
@@ -10,12 +10,18 @@ else ()
   set(ffmpeg_shared_args --disable-shared --enable-static)
 endif ()
 
+set(ffmpeg_c_flags "${superbuild_c_flags}")
+set(ffmpeg_ld_flags "${superbuild_ld_flags}")
+
 set(ffmpeg_extra_args "")
 if (WIN32)
   set(ffmpeg_extra_args --cc=cl --toolchain=msvc)
 elseif (APPLE)
   set(ffmpeg_extra_args --cc=clang --install-name-dir=@rpath)
 endif ()
+
+find_program(BASH_EXECUTABLE bash REQUIRED)
+find_program(MAKE_EXECUTABLE make REQUIRED)
 
 superbuild_add_project(ffmpeg
   BUILD_SHARED_LIBS_INDEPENDENT
@@ -28,6 +34,7 @@ superbuild_add_project(ffmpeg
   SPDX_COPYRIGHT_TEXT
     "Copyright (c) the FFmpeg developers"
   CONFIGURE_COMMAND
+    "${BASH_EXECUTABLE}"
     <SOURCE_DIR>/configure
       --prefix=<INSTALL_DIR>
       --disable-all
@@ -40,12 +47,12 @@ superbuild_add_project(ffmpeg
       --pkg-config=${superbuild_pkgconf}
       ${ffmpeg_extra_args}
       ${ffmpeg_shared_args}
-      --extra-cflags=${superbuild_c_flags}
-      --extra-ldflags=${superbuild_ld_flags}
+      "--extra-cflags=${ffmpeg_c_flags}"
+      "--extra-ldflags=${ffmpeg_ld_flags}"
   BUILD_COMMAND
-    $(MAKE)
+    "${MAKE_EXECUTABLE}"
   INSTALL_COMMAND
-    make install
+    "${MAKE_EXECUTABLE}" install
   BUILD_IN_SOURCE 1
   PROCESS_ENVIRONMENT
     PKG_CONFIG_ALLOW_SYSTEM_CFLAGS 1
